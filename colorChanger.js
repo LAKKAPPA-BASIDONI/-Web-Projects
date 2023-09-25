419823b6496e3020e7350fc48c40fe17cc895f10
lakkappa basidoni <lakkappabasidoni2000@gmail.com>
2023-09-25 12:37:30 +0530

    add initial file

---
+++ colorChanger.js
@@ -0,0 +1,12 @@
+ function changeBackground1(){
+   document.querySelector("body").style.backgroundColor="red";
+ }
+ function changeBackground2(){
+   document.querySelector("body").style.backgroundColor="yellow";
+ }
+ function changeBackground3(){
+   document.querySelector("body").style.backgroundColor="blue";
+ }
+ function changeBackground4(){
+   document.querySelector("body").style.backgroundColor="green";
+ }
